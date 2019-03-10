4b306ed85b83cc466513baa356d1ad16d9347132
Fabian Daugs <fabian.daugs@smail.th-koeln.de>
2019-03-10 11:39:18 +0100

    implemented loginClient

---
+++ src/components/misc/ErrorAlert.vue
@@ -0,0 +1,34 @@
+<template>
+  <div v-if="data"
+       class="message">
+    <b-alert v-if="data.networkError === null"
+             show
+             dismissible
+             variant="warning">
+      <span>
+        {{ data.graphQLErrors[0].message }}
+      </span>
+    </b-alert>
+    <b-alert v-else
+             show
+             dismissible
+             variant="danger">
+      <span>
+        {{ data.message }}
+      </span>
+    </b-alert>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ErrorAlert',
+  props: {
+    data: Error,
+    variant: {
+      type: String,
+      default: 'warning',
+    },
+  },
+}
+</script>
---
+++ src/components/misc/SuccessAlert.vue
@@ -0,0 +1,25 @@
+<template>
+  <b-alert :show="show"
+           dismissible
+           variant="success">
+    {{ message }}
+  </b-alert>
+</template>
+
+<script>
+export default {
+  name: 'SuccessAlert',
+  props: {
+    show: {
+      type: Boolean,
+      required: true,
+      default: true,
+    },
+    message: {
+      type: String,
+      required: false,
+      default: 'Form submission successful',
+    }
+  },
+}
+</script>
