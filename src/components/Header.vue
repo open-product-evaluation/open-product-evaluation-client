8d8212ebe45d0e3794337f435577af85f68055c5
Corinna Klein <corinna_sofie.klein@smail.fh-koeln.de>
2019-03-22 17:45:34 +0100

    changed header to navbar, added navitems + modal

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,24 +1,52 @@
 <template>
-<b-row class="header">
-    <b-col class="header" cols="3">
-        <v-icon class="icon" name="edit" ></v-icon>
-        <span class="comp">OPE</span>
-        </b-col>
-        </b-row>
+<div>
+    <b-navbar toggleable="lg" type="dark" variant="info" class="py-1 pl-2">
+    <b-navbar-brand @click="goHome()">
+        <v-icon class="icon logo" name="edit"></v-icon>
+            <span class="comp logo">OPE</span>
+    </b-navbar-brand>
+    <b-navbar-nav>
+        <b-nav-item @click="goHome()">Home</b-nav-item>
+      </b-navbar-nav>
+    </b-navbar>
+<b-modal centered 
+    ref="warningModal"
+    title="Warning"
+    @ok="changeRouteHome()">
+      <div class="d-block text-center">
+        <h4>The survey is stopped.</h4>
+        <h4>Your votes will not be saved!</h4>
+      </div>
+    </b-modal>
+  </div>
 </template>
 
 <script lang="ts">
 export default {
   name: 'Header',
+  methods: {
+      goHome(this: any) {
+          // Open Modal if User answers a survey
+          if (this.$route.name=='question') {
+              this.$refs.warningModal.show();
+          }
+      },
+      hideModal(this: any) {
+          this.$refs.warningModal.hide();
+      },
+      changeRouteHome(this: any){
+          this.$refs.warningModal.hide();
+          this.$router.replace('/');
+      }
+  }
 };
 </script>
 <style scoped lang="scss">
-.row {
-    margin: 0;
+.bg-info {
+    background: #637d8d !important ;
 }
-.header {
-    background: #637d8d;
-    padding: 3px 0;
+.logo {
+    cursor: pointer;
 }
 .icon {
     color: #ffaa66;
@@ -26,7 +54,6 @@ export default {
     width: 30px;
 }
 .comp {
-    vertical-align: bottom;
     color: #ffaa66;
 }
 </style>
